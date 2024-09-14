8c8ede7d4c8f37c4e061572b3a07493487bee837
ptdewey <patrickdewey2002@gmail.com>
2024-09-13 22:51:27 -0400

    feat: added prompt gen

--- internal/prompt.go
+++ internal/prompt.go
@@ -1,11 +1,26 @@
 package internal
 
-// TODO:
-func genImagePrompt(Config) (string, error) {
-	return "", nil
+import (
+	"fmt"
+	"math/rand"
+)
+
+func GenImagePrompt(cfg Config) string {
+	subject := cfg.PromptConfig.ImagePromptSubject
+	action := cfg.PromptConfig.ImagePromptAction
+
+	prompt_subject := subject[rand.Intn(len(subject))]
+	prompt_action := action[rand.Intn(len(action))]
+
+	return fmt.Sprintf("Generate me an ad for %s where %s is %s", prompt_subject, prompt_subject, prompt_action)
 }
 
-// TODO:
-func genBlogPrompt(Config) (string, error) {
-	return "", nil
+func GenTextPrompt(cfg Config) string {
+	subject := cfg.PromptConfig.TextPromptSubject
+	action := cfg.PromptConfig.TextPromptAction
+
+	prompt_subject := subject[rand.Intn(len(subject))]
+	prompt_action := action[rand.Intn(len(action))]
+
+	return fmt.Sprintf("Write me a blog post about %s and how they %s", prompt_subject, prompt_action)
 }
