543156b680b4e35ba20b7063bf6b35fe70ffeee0
CM360 <31625347+cm-360@users.noreply.github.com>
2024-09-14 09:34:30 -0400

    implement post id gen and text saver util

--- internal/utils.go
+++ internal/utils.go
@@ -2,8 +2,36 @@ package internal
 
 import (
 	"fmt"
+	"io"
+	"math/rand"
 	"os"
 )
+
+func GenUniqueId() string {
+	randomNumber := rand.Uint32()
+
+	// Format the number as an 8-digit hexadecimal
+	postId := fmt.Sprintf("%08x", randomNumber)
+
+	return postId
+}
+
+func SaveText(text string, textFilename string) {
+	// Create or open the file for writing
+	file, err := os.Create(textFilename)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Write the text to the file
+	_, err = io.WriteString(file, text)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+}
 
 func SaveImage(imageData []byte, imageFilename string) {
 	file, err := os.Create(imageFilename)
