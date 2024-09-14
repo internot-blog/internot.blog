7b0f51767b0edd2bf1dcbdd103cf6e3ef4fe12c8
CM360 <31625347+cm-360@users.noreply.github.com>
2024-09-14 00:12:06 -0400

    put some demo code in main

--- main.go
+++ main.go
@@ -9,5 +9,28 @@ import (
 func main() {
 	cfg := internal.GetConfig("./proompt.json")
 
-	fmt.Println(internal.GenTextPrompt(cfg))
+	llmPrompt := internal.GenTextPrompt(cfg)
+	fmt.Println("Prompt:", llmPrompt)
+
+	response, err := internal.GenText(cfg, llmPrompt)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(response)
+
+	imagePrompt := llmPrompt
+	width := 512
+	height := 512
+	steps := 150
+	guidance := 7.5
+
+	imageData, err := internal.GenImage(cfg, imagePrompt, width, height, steps, guidance)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	internal.SaveImage(imageData, "generated_image.png")
 }
